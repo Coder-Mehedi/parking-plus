9666da8ce71fac7f1acfb430959eae5286186fb7
Coder-Mehedi <mehedi5051@gmail.com>
2019-07-07 13:09:57 +0600

    responsive

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-$(document).ready(function() {
-    // $('#menuBtn').on('click', () => {
-    //     console.log('clicked');
-    // });
-
+$(() => {
     /* Hamburger menu animation */
-    $('.menu-btn').click(function() {
-
-        $(this).toggleClass('open');
+    $('.menu-btn').on('click', () => {
+        $('.mobile-menu').toggleClass('open');
+        // $('.mobile-menu').slideToggle();
     });
-
-    /* Menu fade/in out on mobile */
-    $(".menu-btn").click(function(e) {
-        e.preventDefault();
-        $(".mobile-menu").toggleClass('open');
-    });
-
 });
