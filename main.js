6ab862b195240ebd2dce91d3c3d56a552ffee854
Coder-Mehedi <mehedi5051@gmail.com>
2019-07-08 17:09:52 +0600

    Almost Complete

--- main.js
+++ main.js
@@ -4,4 +4,27 @@ $(() => {
         $('.mobile-menu').toggleClass('open');
         // $('.mobile-menu').slideToggle();
     });
+
+
 });
+$(window).scroll(() => {
+    if ($(window).scrollTop() > 510) {
+        $('.white-car').addClass('white-car-animation');
+        $('.red-car').addClass('red-car-animation');
+    } else {
+        $('.white-car').removeClass('white-car-animation');
+        $('.red-car').removeClass('red-car-animation');
+    }
+
+    if ($(window).scrollTop() > 1075) {
+        $('.park-car').addClass('park-car-animation');
+        $('.bar').addClass('bar-animation');
+        $('.orange-car').addClass('orange-car-animation')
+    } else {
+        $('.park-car').removeClass('park-car-animation');
+        $('.bar').removeClass('bar-animation');
+        $('.orange-car').removeClass('orange-car-animation')
+    }
+
+
+})
